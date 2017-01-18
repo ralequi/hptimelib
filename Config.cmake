58c94b696949ba2c3e6d8e41baa2f92045ebe993
Rafael Leira Osuna <rafael.leira@uam.es>
2017-01-18 16:17:03 +0100

    typo in config

--- Config.cmake
+++ Config.cmake
@@ -5,7 +5,7 @@
 option(HPTL_DEBUG "Enable HPTL debug text" ON)
 option(HPTL_ONLYLINUXAPI "Disables custom HPTL functionality and compiles as a Linux-Time wrapper" OFF)
 option(HPTL_128b "Compiles in 128b mode" ON)
-option(HPTL_128b_mixed "Compiles in 128b mode, but without (almost) fill the high part" ON)
+option(HPTL_128b_mixed "Compiles in 128b mode, but (almost) without fill the 64b high part" ON)
 
 #set   (HPTL_DEBUG "https" CACHE STRING "The server api URI-protocol")
 
